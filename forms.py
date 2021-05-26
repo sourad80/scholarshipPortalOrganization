2ce70cd77b8fb245855c1b6c6e2e7ae7ab557977
Sourad80 <souradee80@gmail.com>
2021-05-27 01:12:53 +0530

    Added Update and Delete Scholarship

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import Form
 from flask_wtf.file import FileField,file_allowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, FloatField
+from wtforms.fields.simple import TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from model import Student,Organization,Scholarship
 from flask_login import current_user
@@ -39,26 +40,27 @@ class OrgLogin(Form):
 
 class AddSch(Form):
     name = StringField('Name', validators = [ DataRequired() , Length(min = 2, max = 25) ])
-    description = StringField('Description', validators = [ DataRequired() , Length(min = 2, max = 256) ])
+    description = TextAreaField('Description', validators = [ DataRequired() , Length(min = 2, max = 256) ])
     amount= FloatField('Amount',validators=[DataRequired()],default=0.0)
     cls_x_min_per = FloatField('Class X min %',validators=[DataRequired()],default=0.0)
-    cls_xii_min_per = FloatField('Class XII min %',validators=[DataRequired()],default=0.0)
-    cls_ug_min_per = FloatField('UG min %',validators=[DataRequired()],default=0.0)
+    cls_xii_min_per = FloatField('Class XII min %',default=0.0)
+    cls_ug_min_per = FloatField('UG min %',default=0.0)
     submit = SubmitField('ADD')
 
-    def validate_schname(self,name): #Not working
+    def validate_name(self,name): #Not working
         scholarship = Scholarship.query.filter_by(name=name.data).first()
-
+        print(scholarship)
         if scholarship:
             raise ValidationError('Scholarship name already exist!!')
 
 class UpdateSch(Form):
     name = StringField('Name', validators = [ DataRequired() , Length(min = 2, max = 25) ])
-    description = StringField('Description', validators = [ DataRequired() , Length(min = 2, max = 256) ])
+    description = TextAreaField('Description', validators = [ DataRequired() , Length(min = 2, max = 256) ])
     amount= FloatField('Amount',validators=[DataRequired()],default=0.0)
     cls_x_min_per = FloatField('Class X min %',validators=[DataRequired()],default=0.0)
-    cls_xii_min_per = FloatField('Class XII min %',validators=[DataRequired()],default=0.0)
-    cls_ug_min_per = FloatField('UG min %',validators=[DataRequired()],default=0.0)
+    cls_xii_min_per = FloatField('Class XII min %',default=0.0)
+    cls_ug_min_per = FloatField('UG min %',default=0.0)
     submit = SubmitField('Update Scholarship')
 
         # Have to validate
+
--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
 from flask_login.utils import logout_user
+from werkzeug.exceptions import abort
 from app import app,db,bcrypt
 from flask import render_template,redirect,flash,url_for,request
 from forms import OrgRegistration,OrgLogin,AddSch,UpdateSch
@@ -53,31 +54,61 @@ def dashboard():
 
 @app.route('/addSch',methods=['POST','GET'])
 @login_required
-def addSch():
+def add_sch():
     form=AddSch()
     if form.validate_on_submit():
-        scholarship = Scholarship(name = form.name.data,description=form.description.data,cls_x_min_per=form.cls_x_min_per.data,cls_xii_min_per=form.cls_xii_min_per.data,cls_ug_min_per=form.cls_ug_min_per.data,organization_id=current_user.id )
+        scholarship = Scholarship(name = form.name.data,description=form.description.data,amount=form.amount.data,cls_x_min_per=form.cls_x_min_per.data,cls_xii_min_per=form.cls_xii_min_per.data,cls_ug_min_per=form.cls_ug_min_per.data,organization_id=current_user.id )
         db.session.add(scholarship)
         db.session.commit()
         flash(f'Created New Scholarship', 'success')
-        return redirect(url_for('home'))
+        return redirect(url_for('dashboard'))
     return render_template('schAdd.html', title = "Welcome"+current_user.username,form = form)
 
-@app.route('/updateSch',methods=['POST','GET'])
+@app.route('/updateSch/<id>',methods=['POST','GET'])
 @login_required
-def updateSch():
+def update_sch_id(id):
     form=UpdateSch()
+    scholarship = Scholarship.query.filter_by(id=id).first()
     if form.validate_on_submit():
-            scholarship = Scholarship(name = form.name.data,description=form.description.data,cls_x_min_per=form.cls_x_min_per.data,cls_xii_min_per=form.cls_xii_min_per.data,cls_ug_min_per=form.cls_ug_min_per.data,organization_id=current_user.id )
+            scholarship.name = form.name.data
+            scholarship.amount = form.amount.data
+            scholarship.cls_x_min_per = form.cls_x_min_per.data
+            scholarship.cls_xii_min_per = form.cls_xii_min_per.data
+            scholarship.cls_ug_min_per = form.cls_ug_min_per.data
+            scholarship.description = form.description.data
             db.session.commit()
             flash(f'Scholarship Updated Successfully!!', 'success')
-            return redirect(url_for('home'))
+            return redirect(url_for('dashboard'))
     elif request.method == 'GET':
-            scholarship = Scholarship(name = form.name.data,description=form.description.data,cls_x_min_per=form.cls_x_min_per.data,cls_xii_min_per=form.cls_xii_min_per.data,cls_ug_min_per=form.cls_ug_min_per.data,organization_id=current_user.id )
             form.name.data = scholarship.name
             form.amount.data = scholarship.amount
             form.cls_x_min_per.data = scholarship.cls_x_min_per
             form.cls_xii_min_per.data = scholarship.cls_xii_min_per
             form.cls_ug_min_per.data = scholarship.cls_ug_min_per
+            form.description.data = scholarship.description
 
             return render_template("schUpdate.html", title="SchUpdate",form = form)
+
+@app.route('/updateSch',methods=['POST','GET'])
+@login_required
+def update_sch():
+    scholarships = Scholarship.query.filter_by(organization_id=current_user.id)
+    return render_template('viewUpdateScholarship.html',title="Your Scholarships",scholarships = scholarships)
+
+@app.route('/deleteSch/<id>',methods=['POST','GET'])
+@login_required
+def delete_sch_id(id):
+    scholarship = Scholarship.query.get_or_404(id)
+    if scholarship.organization_id != current_user.id:
+        abort(404)
+    else:
+        db.session.delete(scholarship)
+        db.session.commit()
+    flash(f'Scholarship Deleted Successfully!!', 'success')
+    return redirect(url_for('dashboard'))
+
+@app.route('/deleteSch',methods=['POST','GET'])
+@login_required
+def delete_sch():
+    scholarships = Scholarship.query.filter_by(organization_id=current_user.id)
+    return render_template('viewDeleteScholarship.html',title="Your Scholarships",scholarships = scholarships)
