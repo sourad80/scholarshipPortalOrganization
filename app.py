e0e9dabafe7047411b19f5ba206609b2b4a0e4bc
Souradeep Guha Neogi <soura.d80@gmail.com>
2021-06-26 12:56:04 +0530

    Updated Models

--- app.py
+++ app.py
@@ -2,14 +2,12 @@ from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '4b95e49882f86d7397154c929806fe8d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://qrtggwfmstohzl:0a89cf8c918a805453e847147c36aef4fa1206930dbd23038baaa4340ead05e0@ec2-54-211-77-238.compute-1.amazonaws.com:5432/d4h9o0dgsuo00j'
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 
 from routes import *
--- model.py
+++ model.py
@@ -1,15 +1,13 @@
-from app import db, login_manager
+from app import db,login_manager
 from datetime import datetime
 from flask_login import UserMixin
 
-
 @login_manager.user_loader
 def load_student(user_id):
-    return Organization.query.get(int(user_id))
+    return Student.query.get(int(user_id))
 
-
-class Student(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
+class Student(db.Model,UserMixin):
+    id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     phone = db.Column(db.String(13), unique=True, nullable=False)
@@ -18,57 +16,48 @@ class Student(db.Model, UserMixin):
     clx = db.Column(db.String(120))
     clxmarks = db.Column(db.Float)
     clxii = db.Column(db.String(120))
-    clxiimarks = db.Column(db.Float, default=0.0)
+    clxiimarks = db.Column(db.Float,default = 0.0)
     ug = db.Column(db.String(120))
-    ugmarks = db.Column(db.Float, default=0.0)
+    ugmarks = db.Column(db.Float,default = 0.0)
     pg = db.Column(db.String(120))
-    pgmarks = db.Column(db.Float, default=0.0, nullable=False)
+    pgmarks = db.Column(db.Float,default = 0.0,nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    applications = db.relationship(
-        'scholarship_application', backref='student', lazy=True)
+    applications = db.relationship('scholarship_application', backref='student', lazy=True)
 
     def __repr__(self):
         return f"Student('{self.username}', '{self.email}')"
 
-
-class Organization(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
+class Organization(db.Model,UserMixin):
+    id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     phone = db.Column(db.String(13), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    scholarships = db.relationship(
-        'Scholarship', backref='organization', lazy=True)
+    scholarships = db.relationship('Scholarship', backref='organization', lazy=True)
 
     def __repr__(self):
         return f"Organization('{self.username}', '{self.email}')"
 
-
 class Scholarship(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(30), unique=True, nullable=False)
     description = db.Column(db.String(256))
-    amount = db.Column(db.Float)
+    amount= db.Column(db.Float)
     cls_x_min_per = db.Column(db.Float)
     cls_xii_min_per = db.Column(db.Float)
     cls_ug_min_per = db.Column(db.Float)
-    organization_id = db.Column(db.Integer, db.ForeignKey(
-        'organization.id'), nullable=False)
-    applications = db.relationship(
-        'scholarship_application', backref='application', lazy=True)
+    organization_id = db.Column(db.Integer, db.ForeignKey('organization.id'), nullable=False)
+    life = db.Column(db.Integer, default=1)
+    datetime = db.Column(db.DateTime, default=datetime.utcnow())
+    applications = db.relationship('scholarship_application', backref='scholarship', lazy=True)
 
     def __repr__(self):
         return f"Scholarship('{self.name}', '{self.organization_id}','{self.description}','{self.cls_x_min_per}','{self.cls_xii_min_per}','{self.cls_ug_min_per}')"
 
-
 class scholarship_application(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    sch_id = db.Column(db.Integer, db.ForeignKey(
-        'scholarship.id'), nullable=False)
-    stu_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
-    org_id = db.Column(db.Integer, db.ForeignKey(
-        'organization.id'), nullable=False)
-    status = db.Column(db.Integer, nullable=False)
-
-    def __repr__(self):
-        return f"Scholarship('{self.id}', '{self.status}')"
+    id = db.Column(db.Integer,primary_key=True)
+    sch_id = db.Column(db.Integer, db.ForeignKey('scholarship.id'), nullable=False)
+    stu_id = db.Column(db.Integer, db.ForeignKey('student.id'),nullable=False)
+    org_id = db.Column(db.Integer, db.ForeignKey('organization.id'), nullable=False)
+    datetime = db.Column(db.DateTime, default=datetime.utcnow())
+    status = db.Column(db.Integer,nullable=False)
